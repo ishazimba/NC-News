145915b2838ab992522ee349f574b4c4e6b41e4f
Isha Tamang <isha.zimba@gmail.com>
2023-05-10 14:48:00 +0100

    Delete topics.model.js

--- models/topics.model.js
+++
@@ -1,8 +0,0 @@
-const db = require("../db/connection");
-
-exports.selectTopics = (topic) => {
-  let queryString = `SELECT slug, description FROM topics`;
-  return db.query(queryString).then((result) => {
-    return result.rows;
-  });
-};
