68306baeb0c75b615b4ae22fddeb2f5eab8ad6f5
ishazimba <isha.zimba@gmail.com>
2023-05-11 10:38:28 +0100

    ticket 4: get article by its id

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 
-const { getTopics, getEndpoints } = require("./controllers/ncControllers");
+const {
+  getTopics,
+  getEndpoints,
+  getArticleById,
+} = require("./controllers/ncControllers");
 
 app.get("/api/topics", getTopics);
 app.get("/api", getEndpoints);
+app.get("/api/article/:articleId", getArticleById);
+
+app.use((err, request, response, next) => {
+  if (err.code == "22P02") {
+    response.status(400).send({ message: "BAD REQUEST! INVALID ID" });
+  }
+  response.status(err.status).send({ message: err.message });
+});
 
 module.exports = app;
