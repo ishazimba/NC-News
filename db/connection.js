942670e205461ffff935b6bee79ff0b23e5b8f33
ishazimba <isha.zimba@gmail.com>
2023-05-12 14:49:25 +0100

    hosting api

--- db/connection.js
+++ db/connection.js
@@ -10,7 +10,8 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 }
 const config = {};
 if (ENV === "production") {
-  (config.connectionString = process.env.DATABASE_URL), (config.max = 2);
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
